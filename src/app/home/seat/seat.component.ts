60764b056d2ae42e979b943dc780017123a4b047
Jitto Thomas <jittotp@gmail.com>
2020-05-02 12:56:53 +0530

    May-2 commit

--- src/app/home/seat/seat.component.ts
+++ src/app/home/seat/seat.component.ts
@@ -7,11 +7,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./seat.component.css']
 })
 export class SeatComponent implements OnInit {
-
+  public seatArray= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  seatClicked: boolean = false;
+  selectedSeatNos: number[] = [];
   constructor(private router: Router) { }
 
-  ngOnInit(): void {
-   
+  ngOnInit() {
+   this.seatClicked = false;
+   this.selectedSeatNos = [];
+  }
+
+  onSeatSelect(seatIndex, event) {
+    console.log(event);
+    this.seatClicked = true;
+    this.selectedSeatNos.push(seatIndex);
+    console.log(seatIndex);
   }
 
   onButton() {

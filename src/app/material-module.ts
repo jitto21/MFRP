36a5fccaf692b0cd865419fc1eed2f408619f03e
jitto21 <jittotp@gmail.com>
2022-10-28 20:20:52 +0530

    Remove MatTreeModule

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -41,7 +41,6 @@ import {MatTableModule} from '@angular/material/table';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTreeModule} from '@angular/material/tree';
 
 @NgModule({
   exports: [
@@ -86,7 +85,6 @@ import {MatTreeModule} from '@angular/material/tree';
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
-    MatTreeModule,
     PortalModule,
     ScrollingModule,
   ]
